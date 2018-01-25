32855d341bfd95e1dcae193d9873b1742e76b4a4
Seungri Cho <pseudo.circuit@gmail.com>
2018-01-25 16:56:15 +0900

    함수구현 완료

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import re
 
 import os
 import requests
+from bs4 import BeautifulSoup
 
 
 def get_top100_list(refresh_html=False):
@@ -12,8 +13,8 @@ def get_top100_list(refresh_html=False):
         현재 파일(모듈)의 위치를 사용한 상위 디렉토리 경로 (crawler디렉토리):
             os.path.dirname(os.path.abspath(__name__))
 
-        1~50위:   data/chart_realtime_50.html
-        51~100위: data/chart_realtime_100.html
+        1~10위:   data/chart_realtime.html
+    :param refresh_html: True일 경우, 무조건 새 HTML파일을 사이트에서 내려준다.
     :return:
     """
     # 프로젝트 컨테이너 폴더 경로
@@ -29,8 +30,7 @@ def get_top100_list(refresh_html=False):
     os.makedirs(path_data_dir, exist_ok=True)
 
     # 1~50, 50~100위 웹페이지 주소
-    url_chart_realtime = 'http://www.melon.com/chart/index.htm#params%5Bidx%5D=1'
-    # url_chart_realtime_100 = 'https://www.melon.com/chart/index.htm#params%5Bidx%5D=51'
+    url_chart_realtime = 'https://www.melon.com/chart/index.htm'
 
 
     # refresh_html매개변수가 True 일 경우, 무조건 새로 파일을 다운받도록 함
@@ -48,7 +48,7 @@ def get_top100_list(refresh_html=False):
 
 
 
-    # 100위
+    # 100위 하려다가 만거 (참고)
 
     # file_path = os.path.join(path_data_dir, 'chart_realtime_100.html')
     # if not os.path.exists(file_path) or refresh_html:
@@ -62,31 +62,28 @@ def get_top100_list(refresh_html=False):
 
 
 
+    source = open(file_path, 'rt').read()
+    soup = BeautifulSoup(source, 'lxml')
 
+    result = []
+    for tr in soup.find_all('tr', class_=['lst50', 'lst100']):
+        rank = tr.find('span', class_='rank').text
+        title = tr.find('div', class_='rank01').find('a').text
+        artist = tr.find('div', class_='rank02').find('a').text
+        album = tr.find('div', class_='rank03').find('a').text
+        url_img_cover = tr.find('a', class_='image_typeAll').find('img').get('src')
+        # http://cdnimg.melon.co.kr/cm/album/images/101/28/855/10128855_500.jpg/melon/resize/120/quality/80/optimize
+        # .* -> 임의 문자의 최대 반복
+        # \. -> '.' 문자
+        # .*?/ -> '/'이 나오기 전까지의 최소 반복
+        p = re.compile(r'(.*\..*?)/')
+        url_img_cover = re.search(p, url_img_cover).group(1)
 
-
-    # file_path = os.path.join(path_data_dir, 'abc.txt')
-    # print(f'file_path: \n{file_path}')
-
-    # result = []
-    # for tr in soup.find_all('tr', class_='lst50'):
-    #     rank = tr.find('span', class_='rank').text
-    #     title = tr.find('div', class_='rank01').find('a').text
-    #     artist = tr.find('div', class_='rank02').find('a').text
-    #     album = tr.find('div', class_='rank03').find('a').text
-    #     url_img_cover = tr.find('a', class_='image_typeAll').find('img').get('src')
-    #     # http://cdnimg.melon.co.kr/cm/album/images/101/28/855/10128855_500.jpg/melon/resize/120/quality/80/optimize
-    #     # .* -> 임의 문자의 최대 반복
-    #     # \. -> '.' 문자
-    #     # .*?/ -> '/'이 나오기 전까지의 최소 반복
-    #     p = re.compile(r'(.*\..*?)/')
-    #     url_img_cover = re.search(p, url_img_cover).group(1)
-    #
-    #     result.append({
-    #         'rank': rank,
-    #         'title': title,
-    #         'url_img_cover': url_img_cover,
-    #         'artist': artist,
-    #         'album': album,
-    #     })
-    #
+        result.append({
+            'rank': rank,
+            'title': title,
+            'url_img_cover': url_img_cover,
+            'artist': artist,
+            'album': album,
+        })
+    return result
